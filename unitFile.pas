9ab290098f00b785f4022dedfb3b6b72737b7133
mateo <mateoedgardogomez@gmail.com>
2022-11-14 19:05:01 -0300

    14/11/22

--- unitFile.pas
+++ unitFile.pas
@@ -1,6 +1,40 @@
 
-Program unitFile;
-Begin
+Unit ARCHIVOS;
 
-  Var gato : real;
-  End.
+Interface
+
+Const 
+  RUTA = 'C: \Users\Usuario\Desktop\cadastre-simulation\files';
+
+Type 
+  T_DATO = Record
+    NOM: STRING[60];
+    TEL: STRING[15];
+    ESTADO: BOOLEAN;
+  End;
+  T_ARCHIVO = FILE Of T_DATO;
+Procedure CREAR(Var ARCH:T_ARCHIVO);
+Procedure ABRIR(Var ARCH:T_ARCHIVO);
+Procedure CERRAR(Var ARCH:T_ARCHIVO);
+
+Implementation
+Procedure CREAR(Var ARCH:T_ARCHIVO);
+Begin
+  ASSIGN(ARCH,RUTA);
+  REWRITE(ARCH);
+End;
+Procedure ABRIR(Var ARCH:T_ARCHIVO);
+Begin
+  ASSIGN(ARCH,'C:\ARCHIVO.DAT');
+  RESET(ARCH);
+End;
+
+Reset(ARCH);
+
+If IOResult  <> 0 Then Rewrite(ARCH);
+Procedure CERRAR(Var ARCH:T_ARCHIVO);
+Begin
+  CLOSE(ARCH);
+End;
+Begin
+End
